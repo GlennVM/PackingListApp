8c66284c0b4c2f4973f99bbca9d1cc84d0aa43b4
Dean Delanoye <delanoyedean@gmail.com>
2016-08-22 12:25:16 +0200

    Iterator and Composite pattern

---
+++ PackingList/PackingList/Models/TripComponent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackingList.Models
+{
+    public abstract class TripComponent
+    {
+        public string Name { get; protected set; }
+
+        public virtual void Add(TripComponent t)
+        {
+            throw new TripException();
+        }
+
+        public virtual void Remove(TripComponent t)
+        {
+            throw new TripException();
+        }
+
+        public virtual TripComponent GetChild(int i)
+        {
+            throw new TripException();
+        }
+
+        public virtual IEnumerator<TripComponent> CreateIterator()
+        {
+            throw new TripException();
+        }
+    }
+}
---
+++ PackingList/PackingList/Models/TripIterator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackingList.Models
+{
+    public class TripIterator : IEnumerator<TripComponent>
+    {
+        private Stack<IEnumerator<TripComponent>> stack = new Stack<IEnumerator<TripComponent>>();
+
+        public TripIterator(IEnumerator<TripComponent> it)
+        {
+            stack.Push(it);
+        }
+
+        public TripComponent Current
+        {
+            get
+            {
+                IEnumerator<TripComponent> it = stack.Peek();
+                TripComponent component = it.Current;
+
+                if (component is Trip)
+                    stack.Push(component.CreateIterator());
+
+                return component;
+            }
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MoveNext()
+        {
+            if (stack.Count == 0) return false;
+
+            IEnumerator<TripComponent> it = stack.Peek();
+
+            if (!it.MoveNext())
+            {
+                stack.Pop();
+                return MoveNext();
+            }
+            else
+                return true;
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
